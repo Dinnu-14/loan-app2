f38ae5fc4607f8fda8737408cc1fb66843f60a8f
Dinnu-14 <dineswarraoarisela@gmail.com>
2025-03-28 17:42:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,16 @@ clf = joblib.load('loan_data.joblib')
 
 
 st.title('LOAN APP BY DINESWAR')
-g=st.selectbox('Select gender:',['Male','Female'])
-m = st.selectbox('Select marriage status:', ['No', 'Yes'])
-
-ai=st.number_input('Enter monthly income in thousands')
+g=st.selectbox('Select an gender:',['Male','Female'])
+m=st.selectbox('Select marriage status: ',['No','Yes'])
+ai=st.number_input('Enter Loan amout in thousands')
 la=st.number_input('Enter Loan amount thousands')
-gfinal= 0 if g=='Male' else 1
-marriage=0 if m=='No' else 1
+gfinal = 0 if g == 'Male' else 1
+marriage= 0 if  m == 'No' else 1
 
 if st.button('PREDICT'):
     Prediction = clf.predict([[gfinal,marriage,ai,la]])
     if Prediction == 'Y':
         st.text('LOAN IS APPROVED')
     else:
-        st.text('LOAN IS REJECTED')
+        st.text('LOAN IS REJUCTED')
